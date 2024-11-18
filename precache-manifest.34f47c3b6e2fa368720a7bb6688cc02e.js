501947a12317f273a5a278c03817a37bfb0e5f37
Georges Tchankwe <tchankwes@gmail.com>
2024-11-18 13:35:13 -0600

    Updates

---
+++ precache-manifest.34f47c3b6e2fa368720a7bb6688cc02e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4f00852d18b01d60121f9908b22301",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "896b0ca12dca3086e283",
+    "url": "/robofriends/static/css/2.1f505516.chunk.css"
+  },
+  {
+    "revision": "79c61b73b36798e80a70",
+    "url": "/robofriends/static/css/main.0d807f5f.chunk.css"
+  },
+  {
+    "revision": "896b0ca12dca3086e283",
+    "url": "/robofriends/static/js/2.04da0826.chunk.js"
+  },
+  {
+    "revision": "79c61b73b36798e80a70",
+    "url": "/robofriends/static/js/main.a84b3c11.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
